518cb82e6d33de4df3771249d02a6436d31c1a89
André Lunelli <to.lunelli@gmail.com>
2016-08-10 20:21:44 -0300

    compreendido funcionalidade do timer e timertasks

--- src/main/java/br/com/algoritmo_valentao/algoritmo_valentao/App.java
+++ src/main/java/br/com/algoritmo_valentao/algoritmo_valentao/App.java
@@ -2,6 +2,8 @@ package br.com.algoritmo_valentao.algoritmo_valentao;
 
 import java.util.List;
 import java.util.Random;
+import java.util.Timer;
+import java.util.TimerTask;
 import java.util.Vector;
 
 /**
@@ -10,18 +12,36 @@ import java.util.Vector;
  */
 public class App 
 {
-    private List<Processo> processos = new Vector<Processo>();
-    private Processo coordenador;
-    
-    public static void main( String[] args )
-    {                
-        for (int p = 0; p < 10; p++){
-            Random id = new Random();
-            this.processos.add(new Processo(id.nextInt()));
-        }
-    }
-    
-    public Processo eleicao(Processo[] processos){
-        return null;
-    }
+    public static void main(String[] args) {
+    	long tempoConsultarCoordenador = 1000 * 2;
+    	long tempoCriarNovoProcesso = 1000 * 3;
+    	long tempoEliminaProcessoDiferenteCoordenador = 1000 * 5;    	
+    	long tempoEliminaProcessoCoordenador = 1000 * 10;    	
+        Timer timer = new Timer();
+        TimerTask taskConsultarCoordenador = new TimerTask() {			
+			@Override
+			public void run() {
+				System.out.println("consultar coordenador");
+			}
+		};
+		
+		new TimerTask() {
+			
+			@Override
+			public void run() {
+				// TODO Auto-generated method stub
+				
+			}
+		};
+		
+        TimerTask taskCriarNovoProcesso = new TimerTask() {			
+			@Override
+			public void run() {
+				System.out.println("consultar coordenador");
+			}
+		};
+		timer.scheduleAtFixedRate(taskConsultarCoordenador, tempoConsultarCoordenador, tempoConsultarCoordenador);		
+		timer.scheduleAtFixedRate(taskCriarNovoProcesso, tempoConsultarCoordenador, tempoConsultarCoordenador);
+		
+	}
 }

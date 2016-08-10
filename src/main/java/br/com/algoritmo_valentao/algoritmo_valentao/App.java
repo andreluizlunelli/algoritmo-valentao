f700586275b4d21a5ae0f633e5a81dd2ea04da62
Raí Vieira Adriano <Ra Vieira Adriano>
2016-08-09 21:41:03 -0300

    start

--- src/main/java/br/com/algoritmo_valentao/algoritmo_valentao/App.java
+++ src/main/java/br/com/algoritmo_valentao/algoritmo_valentao/App.java
@@ -1,13 +1,27 @@
 package br.com.algoritmo_valentao.algoritmo_valentao;
 
+import java.util.List;
+import java.util.Random;
+import java.util.Vector;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
+    private List<Processo> processos = new Vector<Processo>();
+    private Processo coordenador;
+    
     public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    {                
+        for (int p = 0; p < 10; p++){
+            Random id = new Random();
+            this.processos.add(new Processo(id.nextInt()));
+        }
+    }
+    
+    public Processo eleicao(Processo[] processos){
+        return null;
     }
 }
